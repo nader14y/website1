a96f56824cf9718df60646837fb63668b560ff44
Natan Y <natan@realtyna.com>
2025-12-01 07:53:17 -0500

    In the name of god

---
+++ boxes/script.js
@@ -0,0 +1,10 @@
+
+
+if(document.getElementById("test").hasAttribute("target"))
+{
+    document.getElementById("test").innerHTML = "No Anchor";
+    document.getElementById("test").style.color="#FF000055";
+}
+else {
+    document.write("New page");
+}
---
+++ newweb/script.js
@@ -0,0 +1,1 @@
+document.getElementById("test").style.backgroundColor = "#ff0000"
